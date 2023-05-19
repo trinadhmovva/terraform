e2efe61c54a6e0b4909331306b764711029c96d1
trinadhmovva <128454762+trinadhmovva@users.noreply.github.com>
2023-05-19 13:57:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ access_key = "AKIAWGGT4JPDFQH5FT5J"
 secret_key = "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"
 }
 
-resource "aws_s3_bucket" "latest" {
-bucket = "movvabucket1"
+resource "aws_s3_bucket" "new" {
+bucket = "movvabucket2"
 tags = {
 Name = "dev"
 }
