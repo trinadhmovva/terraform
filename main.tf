ce9377056a86ef7a8875b97ac82f6cf58e41d7ee
trinadhmovva <128454762+trinadhmovva@users.noreply.github.com>
2023-05-19 13:49:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@ region = "us-east-1"
 access_key = "AKIAWGGT4JPDFQH5FT5J"
 secret_key = "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"
 }
-resource "aws_s3_bucket" "latest" {
+
+resource "aws_s3_bucket" "vcube" {
 bucket = "movvabucket1"
 tags = {
 Name = "dev"
